cafc84ddc9f6392959a8ed10e4212e89114c6c8a
Bailey Day <bailey.j.day@gmail.com>
2020-01-22 13:06:54 -0500

    Updated the RPG game to include character classes, variable attack, health, and abilities.

--- rpg-1.py
+++ rpg-1.py
@@ -1,23 +1,79 @@
 import random
+import inspect
 
-class Hero(Character):
-    # hero_health = 0
-    # hero_power = 0
+# Initilization of the main parent character class.
 
+class Character():
     def __init__(self):
-        self.health = random.randint(10, 15)
-        self.power = round(self.health/2)
-        # hero_health = self.health
-        # hero_power = self.power
-
-    def hero_attack(self, goblin):
-        goblin.health -= self.power
+        self.health = random.randint(15, 25)
+        self.power = round(self.health/random.randint(2,3))
 
     def alive(self):
         if self.health > 0:
             return True
         else: 
             return False
+
+    def attack(self, goblin):
+        goblin.health -= self.power
+
+    
+# The main while loop for the battle
+    
+    def choose_action(self):
+        while gus.alive() == True and rus.alive() == True:
+            
+            rus.display_menu()
+            user_input = input()
+            if user_input == "1":
+                chance = random.randint(0,4)
+                shadow_chance = random.randint(0,9)
+                if inspect.isclass(Shadow):
+                    if shadow_chance == 1:
+                        gus.attack(rus)
+                    else:
+                        print("The enemys attack has missed!")
+                        rus.attack(gus)
+                elif chance == 1:
+                    print("You got a critical hit!")
+                    rus.attack(gus)
+                    rus.attack(gus)
+                    gus.attack(rus)
+                elif chance == 2:
+                    print("Your attack missed!")
+                    gus.attack(rus)
+                elif chance == 3:
+                    print("The enemys attack missed!")
+                    rus.attack(gus)
+                else:
+                    rus.attack(gus)
+                    gus.attack(rus)
+                if gus.health <= 0:
+                    print("You have slayed the beast!")
+                if rus.health <= 0:
+                    print("You have been slain. =(")
+            elif user_input == "2":
+                print("You ran like a coward.")
+                gus.attack(rus)
+                if rus.health <= 0:
+                    print("You have been slain. =(")
+                break
+            elif user_input == "3":
+                gus.attack(rus)
+                gus.attack(rus)
+                gus.attack(rus)
+                if rus.health <= 0:
+                    print("You have been slain. =(")
+                
+            else:
+                print("Invalid input. Try again")
+
+            if inspect.isclass(Medic) == True:
+                if chance == 1:
+                    self.health += 2
+                    print("Passive healing activated")
+            
+# Display menu to choose your options.
 
     def display_menu(self):
         print("You have {} health and {} power.".format(rus.health, rus.power))
@@ -28,57 +84,47 @@ class Hero(Character):
         print("2 -- Run from the goblin")
         print("3 -- Do nothing")
 
-    def choose_action(self,opp):
-        while gus.alive() == True and rus.alive() == True:
-            rus.display_menu()
-            user_input = input()
-            if user_input == "1":
-                rus.hero_attack(gus)
-                gus.goblin_attack(rus)
-                if gus.health <= 0:
-                    print("You have slayed the beast!")
-                if rus.health <= 0:
-                    print("You have been slain. =(")
-            elif user_input == "2":
-                print("You ran like a coward.")
-                gus.goblin_attack(rus)
-                if rus.health <= 0:
-                    print("You have been slain. =(")
-                break
-            elif user_input == "3":
-                gus.goblin_attack(rus)
-                gus.goblin_attack(rus)
-                gus.goblin_attack(rus)
-                if rus.health <= 0:
-                    print("You have been slain. =(")
-                
-            else:
-                print("Invalid input. Try again")
+# Child classes available for battle. 
+
+class Hero(Character):
+    def attack(self, goblin):
+        goblin.health -= self.power
 
 class Goblin(Character):
-
-    # goblin_health = 
-    # goblin_power = 
-
-    def __init__(self):
-        self.health = random.randint(5, 10)
-        self.power = round(self.health/3)
-
     def goblin_attack(self, hero):
         hero.health -= self.power
 
-    def alive(self):
-        if self.health > 0:
-            return True
-        else:
-            return False
+class Medic(Character):
+    pass
+
+class Shadow(Character):
+    def __init__ (self):
+        self.health = 1
+        self.power = 3
+
+# The character selection options
+
+def choose_character():
+    user_input = input("Choose your character! \n 1 - Hero \n 2 - Medic \n 3 - Shadow \n 4 - Zombie \n")
+    if user_input == "1":
+        rus = Hero()
+    elif user_input == "2":
+        rus = Medic()
+    elif user_input == "3":
+        rus = Shadow()
+    elif user_input == "4":
+        rus = Zombie()
+    else:
+        print("Incorrect character selection! Goodbye!")
+        exit()
+    return rus
 
 gus = Goblin()
-mike = Goblin()
-rus = Hero()
-# rus.display_menu()
+
+rus = choose_character()         
+
 rus.choose_action()
 
 
-class Character:
+
 
